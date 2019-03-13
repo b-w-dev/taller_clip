3eefd0b2827b4224a14101d9c42abfc37b5fd4f3
Daniel <dani_9_valerdi@hotmail.com>
2019-03-13 14:20:34 -0600

    agregar valores de campos en el show de cotizacion

--- resources/views/cotizacion/index.blade.php
+++ resources/views/cotizacion/index.blade.php
@@ -12,22 +12,22 @@
         </div>
         <div class="card-body">
             <div class="mt-1">
-                <table class="table table-striped table-bordered">
+                <table class="table table-striped table-bordered table-responsive">
                     <thead>
                         <tr class="table-info">
                             <th scope="col">Cliente / Correo</th>
                             <th scope="col">Número de cotización</th>
                             <th scope="col">fecha de creación</th>
                             <th scope="col">Fecha de entrega</th>
-                           {{--  <th scope="col">Ordenes</th>
-                            <th scope="col">Mano de obra</th>
-                            <th scope="col">Varios</th>
-                            <th scope="col">Envios</th> --}}
+                            <th scope="col">Costo Ordenes</th>
+                            <th scope="col">Costo Mano de obra</th>
+                            <th scope="col">Costo Varios</th>
+                            <th scope="col">Costo Envios</th>
                             <th scope="col">Costo proyecto</th>
                             <th scope="col">Ganancia / Incremento</th>
                             <th scope="col">Subtotal</th>
                             <th scope="col">Total</th>
-                            {{-- <th scope="col">Acción</th> --}}
+                            <th scope="col">Acción</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -38,15 +38,15 @@
                                 <td>{{ $cotizacion->nocotizacion }}</td>
                                 <td>{{$cotizacion->fechaactual}}</td>
                                 <td>{{$cotizacion->fechaentrega}}</td>
-                                {{-- <td>${{$cotizacion->totalordenes}}MXN</td>
+                                <td>${{$cotizacion->totalordenes}}MXN</td>
                                 <td>${{$cotizacion->totalmanodeobra}}MXN</td>
                                 <td>${{$cotizacion->totalvarios}}MXN</td>
-                                <td>${{$cotizacion->totalenvios}}MXN</td> --}}
+                                <td>${{$cotizacion->totalenvios}}MXN</td>
                                 <td>${{$cotizacion->totalproyecto}} MXN</td>
-                                <td>{{$cotizacion->ganancia == "0.00" ? "$".$cotizacion->incremento."MXN" : $cotizacion->ganancia." %"}}</td>
+                                <td>{{$cotizacion->incremento == "0.00" ? "$".$cotizacion->incremento." MXN" : $cotizacion->ganancia." %"}}</td>
                                 <td>${{$cotizacion->resultado}}MXN</td>
                                 <td>${{$cotizacion->totalneto}}MXN</td>
-                                {{-- <td><a href="{{ route('cotizacion.show',['cotizacion'=>$cotizacion]) }}" class="btn btn-info">Detalle</a></td> --}}
+                                <td><a href="{{ route('cotizacion.show',['cotizacion'=>$cotizacion]) }}" class="btn btn-info">Detalle</a></td>
 
                             </tr>
                         @endforeach
